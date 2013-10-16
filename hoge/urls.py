9e43abac9727e133d1b9efe9e0521ff0d098da14
Ubuntu <ubuntu@ip-172-31-11-8.ap-northeast-1.compute.internal>
2013-10-16 14:38:51 +0000

    update

--- hoge/urls.py
+++ hoge/urls.py
@@ -7,12 +7,14 @@ from django.contrib import admin
 from django.contrib import admindocs
 admin.autodiscover()
 
+from hoge import settings
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'hoge.views.home', name='home'),
     # url(r'^hoge/', include('hoge.foo.urls')),
-    url(r'^fuga/', hoge.fuga.views.hello),
-    url(r'^piyo/', hoge.piyo.views.hello),
+    url(r'^fuga/', 'hoge.fuga.views.hello'),
+    url(r'^piyo/', 'hoge.piyo.views.hello'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
